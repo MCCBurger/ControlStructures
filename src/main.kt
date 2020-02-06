93ac2e061c3bcc212fa1e4d572fb0be71a77c0fa
Brian <bburger@students.mchenry.edu>
2020-02-06 16:11:25 -0600

    Added Comments

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,15 @@
 fun main() {
     var temp =0.0
 
-
+   //Ask user for input and validate it is in correct range
     println("Please input a temerature between 92.0 and 104.0")
     temp = readLine()!!.toDouble()
-    while (temp <=92.0 || temp>=104){
+    while (temp <92.0 || temp>104){
         println("Incorrect input.......\nPlease input a number between 92.0 and 104")
         temp = readLine()!!.toDouble()
     }
 
+   //Decide which range the temp should fall in...LOW is determined by default if it passes the loop above
     if(temp>99.5)
         println("Your temperature is: "+temp+ " and it is HIGH")
     else if(temp >=97.5)
